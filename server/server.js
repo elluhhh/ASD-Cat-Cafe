c68ca08e3fd08d74384e7e8e3320d29fffc99182
elluhhh <ella.r.gibbs@student.uts.edu.au>
2025-08-21 03:12:01 +1000

    set up for backend and connection to database

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import cats from "./routes/cats.js";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/cats", cats);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
