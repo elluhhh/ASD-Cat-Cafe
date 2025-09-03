80b3d8877318886520db38db581a50b43c8f3e43
elluhhh <ella.r.gibbs@student.uts.edu.au>
2025-09-03 21:20:28 +1000

    moved bookingRoutes

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getBookings, deleteBookings } = require('controllers/bookingController.js');
+const { getBookings, deleteBooking } = require('.../controllers/bookingController.js');
 
 const router = express.Router();
 
