4e02e6b8e181ecf6ae4b4b5881522dd7402510ca
elluhhh <ella.r.gibbs@student.uts.edu.au>
2025-09-04 00:35:03 +1000

    deleted old bookingRoute file

--- server/routes/bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getBookings, deleteBooking } = require('.../controllers/bookingController.js');
-
-const router = express.Router();
-
-router.get('/', getBookings);
-router.post('/', deleteBooking);
-
-module.exports = router;
