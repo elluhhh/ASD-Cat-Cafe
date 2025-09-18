ab75a1f38f4f888139e22e8bdb05b64d80ad2caf
sazzh <100480867+sazzh@users.noreply.github.com>
2025-09-19 00:57:38 +1000

    Add multer setup

--- routes/catRoutes.js
+++ routes/catRoutes.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const { getCats } = require('../controllers/catController.js');
 
+const multer = require('multer');
+const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('cat-display');
+// save uploaded file to folder
+const storage = multer.diskStorage({
+    destination: 'public/uploads',
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
 });
 
 router.get('/', getCats);
+const upload = multer({ storage });
+
+router.post('/add', upload.single('image'), createCat);
+router.get('/', getAllCats);
+router.get('/:id', getCat)
 
 module.exports = router;
