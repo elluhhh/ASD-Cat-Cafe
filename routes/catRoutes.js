d6e5bf3f7da79f73df110e44ca9694e75bc89315
sazzh <100480867+sazzh@users.noreply.github.com>
2025-09-25 20:00:13 +1000

    Add filter by age category

--- routes/catRoutes.js
+++ routes/catRoutes.js
@@ -19,11 +19,27 @@ router.post('/add', upload.single('image'), createCat);
 router.get('/:id', getCat);
 
 router.get('/', async (req, res) => {
-    const { name, gender } = req.query;
+    const { name, age, gender } = req.query;
     let filter = {};
     // case insensitive, name allows partial matches
     if (gender) filter.gender = new RegExp(`^${gender}$`, 'i');
     if (name) filter.name = new RegExp(name, 'i');
+    if (age) {
+        switch (age) {
+            case 'kitten':
+                filter.ageMonths = { $lte: 12 }; // <=1yr
+                break;
+            case 'junior':
+                filter.ageMonths = { $gte: 13, $lte: 36 }; // >1yr <=3yrs
+                break;
+            case 'adult':
+                filter.ageMonths = { $gte: 37, $lte: 95 }; // >3yrs <8yrs
+                break;
+            case 'senior':
+                filter.ageMonths = { $gte: 96 }; // >=8yrs
+                break;
+        }
+    }
 
     const cats = await Cat.find(filter);
     res.render('cat-display', { cats });
