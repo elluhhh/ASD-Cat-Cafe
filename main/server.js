e8bd6b13e6f96afe282be5b35e947c7bc915b1f3
Jean <98675442+jeajq@users.noreply.github.com>
2025-09-29 19:44:30 +1000

    Edited server.js not to include other features

--- main/server.js
+++ main/server.js
@@ -1,36 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const path = require("path");
 //const bookingRoutes = require("./routes/bookingRoutes");
-//const catRoutes = require("./routes/catRoutes.js");
-const adoptionRoutes = require("./routes/adoptionRoute"); // ← ADD THIS
-const path = require('path');
+//onst catRoutes = require("./routes/catRoutes.js");
+const adoptionRoutes = require("./routes/adoptionRoute"); 
 
 const app = express();
 
+// Middleware
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// MongoDB connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/catcafe")
   .then(() => console.log("DB is connected"))
   .catch(err => console.error("DB connection error", err));
 
-app.use(express.static(__dirname + "/public"));
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.set("view engine", "ejs");
-
+// Routes
+//app.use("/cats", require("./routes/catRoute"));
 //app.use("/bookingManagement", bookingRoutes);
 //app.use("/cats", catRoutes);
 app.use("/adoption", adoptionRoutes);
 
-/*app.get("/", (req, res) => {
-  res.render("index");
+app.get('/catprofile', (req, res) => {
+  res.render('catProfile'); 
 });
-*/
 app.get('/', (req, res) => res.redirect('/adoption/request'));
+
+//app.get('/', (req, res) => res.redirect('/adoption/request'));
 
 app.listen(8000, () => {
   console.log("Server is running on http://localhost:8000/");
 });
-
-console.log('Views dir =', app.get('views'));
