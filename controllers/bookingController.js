872c9bbeb92ece6e6ecd95e4e92cf995b2709be3
elluhhh <ella.r.gibbs@student.uts.edu.au>
2025-09-30 02:01:05 +1000

    search retrieves bookings from inputted date regardless of time

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,4 +1,5 @@
 const Booking = require("../models/bookingModel.js");
+const mongoose = require("mongoose");
 
 const getBookings = async (req, res) => {
 	try {
@@ -23,10 +24,16 @@ const findBookings = async (req, res) => {
 		const bookings = await Booking.find().or([
 			{_id: req.body.id},
 			{email: req.body.email},
-			{date_time: req.body.date}
+			{date_time: {
+				$gte: new Date(req.body.date),
+				$lt: new Date(new Date(req.body.date).getTime() + 60 * 60 * 34 * 1000)
+			}}
 		]).exec();
 		res.render("bookingManagement", { bookings });
 	} catch (err) {
+		// add validation checks
+		// valid id
+		// valid date
 		res.status(500).send(err);
 	}
 };
