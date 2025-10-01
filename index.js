0a77cf680e5a89275e3b5b6f7f1aa97d1f69c213
ena <ena.debnath@proton.me>
2025-10-02 08:06:49 +1000

    created index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require("./server");
+
+const PORT = process.env.PORT || 8000;
+
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+}
