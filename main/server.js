16218bcfce11e80317bcc83a35e794a920dc5076
Jean <98675442+jeajq@users.noreply.github.com>
2025-10-02 16:22:56 +1000

    Updated status tracking

--- main/server.js
+++ main/server.js
@@ -16,17 +16,6 @@ app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// MongoDB connection
-/*mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/catcafe")
-  .then(() => console.log("DB is connected"))
-  .catch(err => console.error("DB connection error", err)); */
-
-if (process.env.NODE_ENV !== "test") {
-  mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/catcafe")
-    .then(() => console.log("DB is connected"))
-    .catch(err => console.error("DB connection error", err));
-}
-
 // Routes
 //app.use("/cats", require("./routes/catRoute"));
 //app.use("/bookingManagement", bookingRoutes);
@@ -38,16 +27,49 @@ app.get('/catprofile', (req, res) => {
 });
 app.get('/', (req, res) => res.redirect('/adoption/request'));
 
+const MONGO_URI =
+  "mongodb+srv://admin:cFBUZU6hozSWFbfk@cat-cafe-website.kycc7fg.mongodb.net/cat-cafe?retryWrites=true&w=majority&appName=cat-cafe-website";
+
+const PORT = process.env.PORT || 8000;
+
+(async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("DB is connected");
+    app.listen(PORT, () =>
+      console.log(`Server running at http://localhost:${PORT}/`)
+    );
+  } catch (err) {
+    console.error("Startup error:", err);
+    process.exit(1);
+  }
+})();
+
+
+module.exports = app;
 //app.get('/', (req, res) => res.redirect('/adoption/request'));
+
+// MongoDB connection
+/*mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/catcafe")
+  .then(() => console.log("DB is connected"))
+  .catch(err => console.error("DB connection error", err)); 
+
+if (process.env.NODE_ENV !== "test") {
+  mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/catcafe")
+    .then(() => console.log("DB is connected"))
+    .catch(err => console.error("DB connection error", err));
+}
+
 
 // Only start server if not in test mode
 if (process.env.NODE_ENV !== "test") {
   app.listen(8000, () => {
     console.log("Server is running on http://localhost:8000/");
   });
-}
+}*/
+
 /*app.listen(8000, () => {
   console.log("Server is running on http://localhost:8000/");
-});*/
+});
 
-module.exports = app;
+module.exports = app;*/
