a61791ed79a75f78caa3976a60aef4bab8b78aa1
elluhhh <ella.r.gibbs@student.uts.edu.au>
2025-10-02 12:55:16 +1000

    edited booking routers for editing booking

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const {
 	createBooking,
-    getAvailableTimes
+    getAvailableTimes,
+    updateBooking
 } = require("../controllers/bookingController.js");
 
 const router = express.Router();
 
 router.get("/", getAvailableTimes);
 router.post("/create", createBooking);
+router.post("/edit/:id", updateBooking);
+router.get("/edit/:id", getAvailableTimes);
 
 module.exports = router;
