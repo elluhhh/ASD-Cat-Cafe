8ef5c58027bdfaf2518979b96f278701e2a2fcda
elluhhh <ella.r.gibbs@student.uts.edu.au>
2025-10-05 00:08:46 +1000

    separated booking management functions into own controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,63 +1,4 @@
 const Booking = require("../models/bookingModel.js");
-const mongoose = require("mongoose");
-
-const getBookings = async (req, res) => {
-	try {
-		const bookings = await Booking.find().sort({date_time: 1});
-		res.render("bookingManagement", { bookings });
-	} catch (err) {
-		res.status(500).send(err);
-	}
-};
-
-const deleteBooking = async (req, res) => {
-	try {
-		await Booking.findByIdAndDelete(req.params.id);
-		res.redirect("/bookingManagement");
-	} catch (err) {
-		res.status(500).send(err);
-	}
-};
-
-const findBookings = async (req, res) => {
-	try {
-		const {id, email, date} = req.body
-		var bookings = new Array();
-		var dateObj = new Date(req.body.date);
-
-		if(mongoose.Types.ObjectId.isValid(id)) {
-			bookings.push(await Booking.findById(id));
-		}
-
-		if(email != "") {
-			bookings = bookings.concat(await Booking.find(
-				{email: email}
-			).exec());
-		}
-
-		if(date != "") {
-			bookings = bookings.concat(await Booking.find(
-				{date_time: {
-					$gte: dateObj,
-					$lt: dateObj.getTime() + 24 * 60 * 60 * 1000
-				}}
-			).exec());
-		}
-
-		//remove empty arrays from bookings array
-		bookings = bookings.filter(e=> {
-			if(Array.isArray(e) && e.length == 0){
-				return false;
-			} else {
-				return true;
-			}
-		});
-		
-		res.render("bookingManagement", { bookings });
-	} catch (err) {
-		res.status(500).send(err);
-	}
-};
 
 const updateBooking = async (req, res) => {
 	try {
@@ -88,7 +29,7 @@ const createBooking = async (req, res) => {
 			total_price: total_price,
 			phone: phone
 		});
-		
+
 		res.redirect("/");
 	} catch (err) {
 		res.status(500).send(err);
@@ -134,9 +75,6 @@ const getAvailableTimes = async (req, res) => {
 };
 
 module.exports = {
-	getBookings,
-	deleteBooking,
-	findBookings,
 	updateBooking,
 	createBooking,
 	getAvailableTimes
