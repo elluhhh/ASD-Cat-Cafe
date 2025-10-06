28abf407ca4d24b57885899bd5bbfb0ccaea37be
elluhhh <ella.r.gibbs@student.uts.edu.au>
2025-10-06 17:59:53 +1100

    Revert "Merge remote-tracking branch 'origin/dev' into booking-management"
    This reverts commit 734e35e4ed9559d5e10477ed386c3e23a5d688c3, reversing
    changes made to 535353662944ae1c440a5e845c6698a31eb92c11.

--- routes/adoptionRequestRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAllRequests, getRequestById, updateRequest } = require('../controllers/requestController.js');
-const router = express.Router();
-
-router.get('/', getAllRequests);
-router.get('/:id', getRequestById);
-router.post('/update', updateRequest);
-
-module.exports = router;
