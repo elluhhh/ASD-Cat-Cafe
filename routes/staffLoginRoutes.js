3de91037a6f67f9f0421bbbf94ad7af05586010a
elluhhh <ella.r.gibbs@student.uts.edu.au>
2025-10-17 01:26:32 +1100

    added staff login routes

---
+++ routes/staffLoginRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const {
+    findStaff
+} = require("../controllers/staffController.js");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.render("staffLogin")
+});
+router.post("/", findStaff);
+
+module.exports = router;
