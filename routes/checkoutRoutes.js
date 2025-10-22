ca606eb264d41861f52343ccc6644db771585cec
elluhhh <ella.r.gibbs@student.uts.edu.au>
2025-10-23 05:12:12 +1100

    added booking routes to checkoutRoutes

--- routes/checkoutRoutes.js
+++ routes/checkoutRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { getCheckout, processPayment } = require("../controllers/checkoutController");
+const { getCheckout, getBookingCheckout, processPayment, processBookingPayment } = require("../controllers/checkoutController");
 const router = express.Router();
 
-router.get("/", getCheckout);            
-router.post("/process", processPayment); 
+router.get("/", getCheckout);
+router.get("/:id", getBookingCheckout);
+router.post("/process", processPayment);
+router.post("/process/:id", processBookingPayment);
 
 module.exports = router;
